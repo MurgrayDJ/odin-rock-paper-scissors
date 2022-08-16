ae9bf8b1f449366f3943fd8360bf55dfada5ddfc
MurgrayDJ <murgraydj@gmail.com>
2022-08-16 17:06:59 -0400

    Completes Rock Paper Scissors game
    Adds functionality so that game is played 5 times. Wins, losses, and
    ties are tallied up. Win/lose messages are displayed. Program now
    prevents user from entering invalid responses.

--- game-script.js
+++ game-script.js
@@ -1,3 +1,8 @@
+//Author: Murgray D. John
+//Date: 8/16/2022
+//Purpose: To play 5 rounds of rock, paper, scissors
+
+//Gets the computer to randomly choose an int representing rock, paper, or scissors
 function getComputerChoice(){
     let randInt = Math.floor(Math.random() * 3);
 
@@ -8,21 +13,80 @@ function getComputerChoice(){
     }
 }
 
+//Prompts the user to choose either rock paper, or scissors
+function getUserChoice(){
+    let playerSelection = prompt('Rock, Paper, or Scissors?');
+    let pS = playerSelection.toLowerCase();
+    
+    //Ensures valid entry
+    while(pS != 'rock' && pS != 'scissors' && pS != 'paper'){
+        playerSelection = prompt('Invalid entry: Rock, Paper, or Scissors?');
+        pS = playerSelection.toLowerCase();
+    }
+
+    return playerSelection;
+}
+
+//Plays one round, returns whether it's a tie, win for computer, or win for user
 function playRound(playerSelection, computerSelection) {
     let pS = playerSelection.toLowerCase();
     let cS = computerSelection.toLowerCase();
 
     if(pS === cS){
-        return "It's a tie!";
+        console.log("It's a tie!");
+        return 'tie';
     }
     else if(pS === "rock" && cS === "paper" ||
             pS === "paper" && cS === "scissors" ||
             pS === "scissors" && cS === "rock"){
         
-        return `You lose! ${cS} beats ${pS}!`;
+        console.log(`You lose! ${cS} beats ${pS}!`);
+        return 'computer';
     }
     else{
-        return `You win! ${pS} beats ${cS}!`;
+        console.log(`You win! ${pS} beats ${cS}!`);
+        return 'user';
     }
 }
 
+//Prints out win information, checks who won the most rounds
+function bestOfFive(userWins, computerWins, ties){
+    console.log(`Player wins: ${userWins} Computer wins: ${computerWins} Ties: ${ties}`)
+    
+    if(userWins > computerWins){
+        console.log(`Congrats, you beat the computer ${userWins} time(s). You won!`);
+    }
+    else if (userWins < computerWins){
+        console.log(`Aww, you only won ${userWins} time(s). Better luck next time!`);
+    }
+    else{
+        console.log("Wow it's a complete tie! Try to beat me next time!");
+    }
+}
+
+//Plays five rounds of rock paper scissors. Keeps track of player wins, computer wins,
+//and ties
+function game(){
+    console.log("Let's play Rock, Paper, Scissors!");
+    let userWins = 0;
+    let computerWins = 0;
+    let ties = 0;
+    
+    for(let i = 1; i < 6; i++){
+        console.log(`Round ${i}:`); 
+        let currentWinner = playRound(getUserChoice(), getComputerChoice());
+        if(currentWinner === 'user'){
+            userWins++;
+        }
+        else if (currentWinner === 'computer'){
+            computerWins++;
+        }
+        else{
+            ties++;
+        }
+    }
+
+    bestOfFive(userWins, computerWins, ties);
+}
+
+game();
