4ae9423d676e857251129bdcb554716c472e3f7a
murgraydj <murgraydj@gmail.com>
2022-10-28 13:29:17 -0400

    Reorganizes Javascript functions
    Now functions are in the order that they are called in the file.

--- game-script.js
+++ game-script.js
@@ -4,51 +4,6 @@
 //Purpose: To play 5 rounds of rock, paper, scissors
 
 
-//Gets the computer to randomly choose an int representing rock, paper, or scissors
-function getComputerChoice(){
-    let randInt = Math.floor(Math.random() * 3);
-
-    switch(randInt){
-        case 0: return 'rock';
-        case 1: return 'paper';
-        case 2: return 'scissors';
-    }
-}
-
-//Plays one round, returns whether it's a tie, win for computer, or win for user
-function playRound(playerSelection, computerSelection) {
-    const pS = playerSelection;
-    const cS = computerSelection;
-    const resultMsg = document.getElementById("current-round");
-
-    if(pS === cS){
-        resultMsg.textContent = "It's a tie!";
-        return 'tie';
-    }
-    else if(pS === "rock" && cS === "paper" ||
-            pS === "paper" && cS === "scissors" ||
-            pS === "scissors" && cS === "rock"){
-        
-        resultMsg.textContent = `You lose! ${cS} beats ${pS}!`;
-        return 'computer';
-    }
-    else{
-        resultMsg.textContent = `You win! ${pS} beats ${cS}!`;
-        return 'user';
-    }
-}
-
-//Prints out win information, checks who won the most rounds
-function announceWinner(userWins, computerWins){
-    const winner = document.getElementById("winner");
-
-    if(userWins > computerWins){
-        winner.textContent = `Congratulations, you won!`;
-    }
-    else{
-        winner.textContent = 'The computer won! Better luck next time!';
-    }
-}
 
 //Event listeners for buttons
 const buttons = document.querySelectorAll(".play-btn"); //Nodelist of items with class "play-btn"
@@ -94,5 +49,50 @@ function game(playerSelection){
         }
     }
 
-   
+}
+
+//Plays one round, returns whether it's a tie, win for computer, or win for user
+function playRound(playerSelection, computerSelection) {
+    const pS = playerSelection;
+    const cS = computerSelection;
+    const resultMsg = document.getElementById("current-round");
+
+    if(pS === cS){
+        resultMsg.textContent = "It's a tie!";
+        return 'tie';
+    }
+    else if(pS === "rock" && cS === "paper" ||
+            pS === "paper" && cS === "scissors" ||
+            pS === "scissors" && cS === "rock"){
+        
+        resultMsg.textContent = `You lose! ${cS} beats ${pS}!`;
+        return 'computer';
+    }
+    else{
+        resultMsg.textContent = `You win! ${pS} beats ${cS}!`;
+        return 'user';
+    }
+}
+
+//Gets the computer to randomly choose an int representing rock, paper, or scissors
+function getComputerChoice(){
+    let randInt = Math.floor(Math.random() * 3);
+
+    switch(randInt){
+        case 0: return 'rock';
+        case 1: return 'paper';
+        case 2: return 'scissors';
+    }
+}
+
+//Prints out win information, checks who won the most rounds
+function announceWinner(userWins, computerWins){
+    const winner = document.getElementById("winner");
+
+    if(userWins > computerWins){
+        winner.textContent = `Congratulations, you won!`;
+    }
+    else{
+        winner.textContent = 'The computer won! Better luck next time!';
+    }
 }
